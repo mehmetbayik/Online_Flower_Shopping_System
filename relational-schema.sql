fc9ad0880a41e3703a209337f0a24c7df5122a02
mehmetb35 <mehmetb35@hotmail.com>
2024-04-16 21:02:29 +0300

    update relational-schema.sql

--- relational-schema.sql
+++ relational-schema.sql
@@ -1,123 +1,59 @@
 -- SQL Relational Schema
--- Example Syntax:
-
-
 
 -- User and its IS-A relationships
 CREATE TABLE User(
-   ID CHAR(10) NOT NULL,
+   ID INT NOT NULL UNIQUE,
    firstname CHAR(20),
    lastname CHAR(20),
    phone_number INT,
-   Upassword CHAR(20),
-   email CHAR(20),
+   Upassword CHAR(20) UNIQUE,
+   email CHAR(20) UNIQUE,
    PRIMARY KEY(ID)
 );
 
-
-User {
-	ID-Numeric-Not Null
-	firstname-Text
-	lastname-Text
-	phone_number-Numeric
-	Upassword-Text
-	email-Text
-	Primary Key (ID)
-}
-
-
 CREATE TABLE Customer(
-   CID CHAR(10) NOT NULL,
+   CID INT NOT NULL UNIQUE,
    Caddress CHAR(20),
-   creditcard_no CHAR(20),
+   creditcard_no INT,
    PRIMARY KEY (CID),
    FOREIGN KEY (CID) REFERENCES User(ID)
 );
 
-
-Customer {
-	CID-Numeric-Not Null
-	Caddress-Text
-	creditcard_no-Numeric
-	Primary Key (CID)
-	Foreign Key (CID) -> User (ID)
-}
-
-
 CREATE TABLE Admin(
-   AID CHAR(10) NOT NULL,
-   ssn CHAR(20),
+   AID INT NOT NULL UNIQUE,
+   ssn INT,
    PRIMARY KEY (AID),
    FOREIGN KEY (AID) REFERENCES User(ID)
 );
 
-
-Admin {
-	AID-Numeric-Not Null
-	ssn-Numeric
-	Primary Key (AID)
-	Foreign Key (AID) -> User (ID)
-}
-
-
 CREATE TABLE Deliverer(
-   DID CHAR(10) NOT NULL,
-   ssn CHAR(20),
+   DID INT NOT NULL UNIQUE,
+   ssn INT,
    PRIMARY KEY (DID),
    FOREIGN KEY (DID) REFERENCES User(ID)
 );
-
-
-Deliverer {
-	DID-Numeric-Not Null
-	ssn-Numeric
-	Primary Key (DID)
-	Foreign Key (DID) -> User (ID)
-}
 	
 -- Other Attributes
 
 
-CREATE TABLE Discount(
-   discount_code CHAR(20) NOT NULL,
-   discount_perc INT,
-   Sdate DATE,
-   Edate DATE,
-   PRIMARY KEY (discount_code)
-);
-
-
-Discount {
-	discount_code-Text-Not Null
-discount_perc -Numeric
-	Sdate -Date
-Edate -Date
-	Primary Key (discount_code)
-	}
-
-
 CREATE TABLE Orders(
    OrderID CHAR(10) NOT NULL,
+   Placing_ID INT NOT NULL,
+   Delivering_ID INT NOT NULL,
+   Containing_ID INT NOT NULL,
    order_date DATE,
    delivery_date DATE,
    paid_price INT,
    gift_note CHAR(20),
    PRIMARY KEY (OrderID)
+   FOREIGN KEY (Placing_ID) REFERENCES Customer(CID)
+   FOREIGN KEY (Delivering_ID) REFERENCES Deliverer(DID)
+   FOREIGN KEY (Containing_ID) REFERENCES Flower_arrangement(FID)
+
 );
-Orders{
-OrderID -Text-Not Null
-	order_date -Date
-delivery_date -Date
-	paid_price -Numeric
-gift_note -Text
-	Primary Key (OrderID )
-	}
-
-
-
 
 CREATE TABLE Flower_arrangement(
-   FID CHAR(10) NOT NULL,
+   FID CHAR(10) NOT NULL UNIQUE,
    Fname CHAR(20),
    price INT,
    quantity INT,
@@ -127,58 +63,31 @@ CREATE TABLE Flower_arrangement(
    PRIMARY KEY (FID)
 );
 
-
-
-
+CREATE TABLE Discount(
+   discount_code CHAR(20) NOT NULL UNIQUE,
+   Entering_ID INT NOT NULL,
+   discount_perc INT,
+   Sdate DATE,
+   Edate DATE,
+   PRIMARY KEY (discount_code)
+   FOREIGN KEY (Entering_ID) REFERENCES Customer(CID)
+);
 -- Relation Tables
 
-
 CREATE TABLE enters(
-   CID CHAR(10) NOT NULL,
-   discount_code CHAR(20) NOT NULL,
+   CID INT NOT NULL UNIQUE,
+   discount_code CHAR(20) NOT NULL UNIQUE,
    PRIMARY KEY (discount_code),
    FOREIGN KEY (CID) REFERENCES Customer(CID),
    FOREIGN KEY (discount_code) REFERENCES Discount(discount_code)
 );
 
-
-
-
-CREATE TABLE contains(
-   OrderID CHAR(10) NOT NULL,
-   FID CHAR(10) NOT NULL,
-   PRIMARY KEY (OrderID),
-   FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-   FOREIGN KEY (FID) REFERENCES Flower_arrangement(FID)
-);
-
-
-
-
-CREATE TABLE delivers(
-   OrderID CHAR(10) NOT NULL,
-   DID CHAR(10) NOT NULL,
-   PRIMARY KEY (OrderID),
-   FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-   FOREIGN KEY (DID) REFERENCES Deliverer(DID) -- Or User?
-);
-
-
-CREATE TABLE places(
-   OrderID CHAR(10) NOT NULL,
-   CID CHAR(10) NOT NULL,
-   PRIMARY KEY (OrderID),
-   FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-   FOREIGN KEY (CID) REFERENCES Customer(CID) -- Or User?
-);
-
-
 CREATE TABLE updates(
-   OrderID CHAR(10) NOT NULL,
-   DID CHAR(10) NOT NULL,
+   OrderID CHAR(10) NOT NULL UNIQUE,
+   DID CHAR(10) NOT NULL UNIQUE,
    Ustatus BOOLEAN,
    PRIMARY KEY (OrderID, DID),
    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-   FOREIGN KEY (DID) REFERENCES Deliverer(DID) -- Or User?
+   FOREIGN KEY (DID) REFERENCES Deliverer(DID)
 );
 
